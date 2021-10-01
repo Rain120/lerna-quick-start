6c51ea3c1c24afdd20e35a116169329cce72f394
Rain120 <1085131904@qq.com>
2021-10-01 13:56:01 +0800

    feat: rebuild site

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,19 +1,38 @@
+import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Card, Layout, List, PageHeader, Typography } from 'antd';
+import Link from 'next/link';
+import styles from 'styles/common.module.scss';
 
-const { Content, Footer, Header } = Layout;
-const { Text, Title } = Typography;
-
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <Layout className="layout">
+    <div className={styles.container}>
       <Head>
         <title>Lerna Quick Start</title>
+        <meta name="description" content="Lerna Quick Start" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Content>
-        <Title>Welcome to Here</Title>
-      </Content>
-    </Layout>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <span className={styles.highlight}>Lerna Quick Start</span>
+        </h1>
+
+        <p className={styles.description}>Lerna Quick Start.</p>
+
+        <div className={styles.grid}>
+          <a href={'/playground/demo'} className={styles.card}>
+            <h2>Playground &rarr;</h2>
+            <p>Play it.</p>
+          </a>
+
+          <a href="#" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>For Developer.</p>
+          </a>
+        </div>
+      </main>
+    </div>
   );
-}
+};
+
+export default Home;
